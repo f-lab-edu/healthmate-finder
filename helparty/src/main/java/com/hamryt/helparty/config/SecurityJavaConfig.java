e8866b3c7727413f8f56d7d2738d222b1b4364b3
hamryt <rkdl0829@naver.com>
2021-02-25 14:13:25 +0900

    reate join service
    to make join service I made 6 packages for categorise class.
    1. config
    (DatabaseConfiguration: setting mybatis configuration,
    SecurityJavaConfig: for connecting with mysql )
    2.contoller
    2.contoller
    (UserController: RestApi for Post /users)
    3.dto
    3.dto
    (UserDto: setting join  user information)
    4. exception
    (EmailExistedException: when other join user has same email then throw
    this class exception
    UnexpectedInsertException: when inserting not acted then throw this
    class exception)
    5. mapper
    (UserMapper: mapper for userDto and UserMapper.xml)
    6. service
    (UserServiceImpl: has business logic about user
    UserService: interface for UserServiceImpl)

--- helparty/src/main/java/com/hamryt/helparty/config/SecurityJavaConfig.java
+++ helparty/src/main/java/com/hamryt/helparty/config/SecurityJavaConfig.java
@@ -1,12 +1,9 @@
 package com.hamryt.helparty.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,15 +12,8 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .csrf().disable()
-        .cors().disable()
-        .formLogin().disable()
-        .headers().frameOptions().disable();
-  }
+        .csrf().disable();
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
   }
 
 }
