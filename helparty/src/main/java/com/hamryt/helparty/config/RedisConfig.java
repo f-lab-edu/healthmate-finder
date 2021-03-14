722e291df64ab766210b0b61459fd292e0e12a77
hamryt <rkdl0829@naver.com>
2021-03-14 19:03:33 +0900

    안쓰는 import 삭제

--- helparty/src/main/java/com/hamryt/helparty/config/RedisConfig.java
+++ helparty/src/main/java/com/hamryt/helparty/config/RedisConfig.java
@@ -1,7 +1,5 @@
 package com.hamryt.helparty.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
