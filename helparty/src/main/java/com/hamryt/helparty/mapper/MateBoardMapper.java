f9e1b9406bece34f272379be889e124050faa779
hamryt <rkdl0829@naver.com>
2021-04-12 01:23:44 +0900

    동행 게시물 조회 API 개발

--- helparty/src/main/java/com/hamryt/helparty/mapper/MateBoardMapper.java
+++ helparty/src/main/java/com/hamryt/helparty/mapper/MateBoardMapper.java
@@ -1,11 +1,14 @@
 package com.hamryt.helparty.mapper;
 
 import com.hamryt.helparty.dto.mateboard.MateBoardDTO;
+import com.hamryt.helparty.dto.mateboard.response.GetMateBoardResponse;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MateBoardMapper {
 
     int insertMateBoard(MateBoardDTO mateBoard);
-
+    
+    List<GetMateBoardResponse> findMateBoardByPage(int page, int size);
 }
