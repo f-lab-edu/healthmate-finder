d21901019182abdaf2bb84dbf444dabd6b2cd32c
hamryt <rkdl0829@naver.com>
2021-05-05 16:49:25 +0900

    테스트 코드 요청 객체 jackson을 사용하여 간소화 및 오타 수정

--- helparty/src/test/java/com/hamryt/helparty/controller/GymControllerTest.java
+++ helparty/src/test/java/com/hamryt/helparty/controller/GymControllerTest.java
@@ -6,9 +6,13 @@ import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hamryt.helparty.dto.UserType;
+import com.hamryt.helparty.dto.gym.request.SignUpGymRequest;
 import com.hamryt.helparty.dto.gym.response.SignUpGymResponse;
+import com.hamryt.helparty.dto.user.request.SignUpUserRequest;
 import com.hamryt.helparty.service.gym.GymServiceImpl;
+import java.io.File;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,7 +34,7 @@ class GymControllerTest {
     private GymServiceImpl gymService;
     
     @Test
-    @DisplayName("운동시설 관리자 계정 생성 성공하면 해당 계정 정보르 반환한다.")
+    @DisplayName("운동시설 관리자 계정 생성 성공하면 해당 계정 정보를 반환한다.")
     public void create_Success() throws Exception {
         
         String email = "test@example.com";
@@ -44,17 +48,34 @@ class GymControllerTest {
         SignUpGymResponse signUpGymResponse =
             getSignGymResponse(1004L, email, gymName, phoneNumber, addressCode, addressDetail,
                 userType);
-        
+    
+        SignUpGymRequest signUpGymRequest =
+          getSignGymRequest(email, gymName, password, phoneNumber, addressCode, addressDetail, userType);
+    
+        ObjectMapper mapper = new ObjectMapper();
+    
+        String request = mapper.writeValueAsString(signUpGymRequest);
         given(gymService.insertGym(any())).willReturn(signUpGymResponse);
         
         mvc.perform(post("/gyms")
             .contentType(MediaType.APPLICATION_JSON)
-            .content(
-                "{ \"email\":\"test@example.com\", \"gymName\":\"testGym\", \"password\":\"123\", \"phoneNumber\":\"01012345678\", \"addressCode\":\"1234\", \n"
-                    + " \"addressDetail\":\"Seoul\", \"userType\":\"GYM\"}"))
+            .content(request))
             .andExpect(status().isCreated());
         
         verify(gymService).insertGym(any());
+    }
+    
+    private SignUpGymRequest getSignGymRequest(String email, String gymName, String password,
+      String phoneNumber, String addressCode, String addressDetail, UserType userType) {
+        return SignUpGymRequest.builder()
+          .email(email)
+          .gymName(gymName)
+          .password(password)
+          .phoneNumber(phoneNumber)
+          .addressCode(addressCode)
+          .addressDetail(addressDetail)
+          .userType(userType)
+          .build();
     }
     
     private SignUpGymResponse getSignGymResponse(Long id, String email, String gymName,
