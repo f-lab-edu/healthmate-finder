e1a87f43cc193c02c63c14ed551507af26f495b1
hamryt <rkdl0829@naver.com>
2021-05-06 17:36:55 +0900

    운동시설 관리자 로그인 시 계정 없을 때 발생하는 예외에 대한 테스트 코드를 GymServiceImplTest에서 작성. 해당 예외에 대한 의존성을 GymService에서 갖고 있기 때문에 LoginService가 아닌 GymService에서 작성하였습니다.

--- helparty/src/test/java/com/hamryt/helparty/service/login/LoginServiceImplTest.java
+++ helparty/src/test/java/com/hamryt/helparty/service/login/LoginServiceImplTest.java
@@ -2,15 +2,12 @@ package com.hamryt.helparty.service.login;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
 import com.hamryt.helparty.dto.UserType;
 import com.hamryt.helparty.dto.gym.GymDTO;
 import com.hamryt.helparty.exception.common.UserTypeDoesNotMatchException;
-import com.hamryt.helparty.exception.gym.GymNotFoundException;
-import com.hamryt.helparty.mapper.GymMapper;
 import com.hamryt.helparty.service.gym.GymServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,9 +28,6 @@ class LoginServiceImplTest {
     
     @Mock
     private Encryptor encryptor;
-    
-    @Mock
-    private GymMapper gymMapper;
     
     @Mock
     private MockHttpSession session;
@@ -67,20 +61,6 @@ class LoginServiceImplTest {
         GymDTO gymDTO = loginService.loginGym(email, password);
         
         assertEquals(gymDTO.getEmail(), email);
-    }
-    
-    @Test
-    @DisplayName("운동시설 관리자 로그인 실패 : 로그인 정보 유저 없음 예외")
-    public void loginGym_Fail_GymNotFoundException() {
-        
-        given(gymMapper.findGymByEmailAndPassword(any(), any())).willReturn(null);
-        
-        GymNotFoundException gymNotFoundException
-            = assertThrows(GymNotFoundException.class,
-            () -> loginService.loginGym(email, password));
-        
-        assertEquals("This gym does not exists with this email : test@example.com",
-            gymNotFoundException.getMessage());
     }
     
     @Test
