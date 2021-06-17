4793e881cc8d64273d046e9128c9dbb86e04a614
hamryt <rkdl0829@naver.com>
2021-06-18 03:55:56 +0900

    DB Replication 설정

---
+++ helparty/src/main/java/com/hamryt/helparty/util/DatabaseType.java
@@ -0,0 +1,9 @@
+package com.hamryt.helparty.util;
+
+public class DatabaseType {
+    
+    public static final String MASTER = "master";
+    
+    public static final String SLAVE = "slave";
+    
+}
